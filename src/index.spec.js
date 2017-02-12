5723861666f3039fa6559ac2754ca11af5b71dcc
Sarel Esterhuizen <sarel.esterhuizen@gmail.com>
2017-02-12 08:00:14 +0200

    Implemented reference test for jsdom

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,23 @@
 import { expect } from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs'; // Node File System
 
+// Example unit test showing how to test using mocha with chai assertions.
 describe('Example Unit Test', () => {
   it('Should Pass', () => {
     expect(true).to.equal(true);
   });
+});
+
+// Example unit test showing hot to test using jsdom.
+describe('index.html', () => {
+  it('Should Say Go Forth', (done) => { // Whenever you do an async test add the done parameter.
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    jsdom.env(index, function (err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal('Go forth and multiply..');
+      done(); // Need to do this to indicate whether it is safe to evaluate.
+      window.close();
+    });
+  })
 });
